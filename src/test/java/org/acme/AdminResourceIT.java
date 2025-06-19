515eb8cedfc2efb3afc03c9b98b13dee19b34600
Markus Eisele <markus@redhat.com>
2025-06-19 20:21:51 +0200

    FixTests

--- src/test/java/org/acme/AdminResourceIT.java
+++ src/test/java/org/acme/AdminResourceIT.java
@@ -1,8 +1,11 @@
 package org.acme;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 class AdminResourceIT extends AdminResourceTest {
     // tests are inherited
 }
--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -1,8 +1,11 @@
 package org.acme;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
